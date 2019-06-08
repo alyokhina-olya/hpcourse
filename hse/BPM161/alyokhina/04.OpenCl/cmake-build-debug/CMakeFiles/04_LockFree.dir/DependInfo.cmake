c9b95d4921f5985100ca9e5a0b8b9bda0463a52d
Olga Alekhina <alyokhina-o@yandex-team.ru>
2019-06-08 22:42:37 +0300

    rename

---
+++ hse/BPM161/alyokhina/04.OpenCl/cmake-build-debug/CMakeFiles/04_LockFree.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alyokhina-o/hpcourse/hse/BPM161/alyokhina/04.LockFree/main.cpp" "/Users/alyokhina-o/hpcourse/hse/BPM161/alyokhina/04.LockFree/cmake-build-debug/CMakeFiles/04_LockFree.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hse/BPM161/alyokhina/04.OpenCl/cmake-build-debug/CMakeFiles/04_LockFree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/04_LockFree.dir/main.cpp.o"
+  "04_LockFree.pdb"
+  "04_LockFree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04_LockFree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
